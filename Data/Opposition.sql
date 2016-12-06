2060bcd30f845ae1beec57c0f54363cecc511434
daniel.gasson <daniel.gasson@cots.local>
2016-12-06 19:02:01 +0000

    Add Season 3 Stats

--- Data/Opposition.sql
+++ Data/Opposition.sql
@@ -1,11 +1,16 @@
 INSERT IGNORE INTO Cricket.Opposition (OppositionId, ParentOppositionId, Name)
 VALUES
-(1, null, 'Out of Control'),
-(2, null, 'Designated Drinkers'),
-(3, null, 'The Pirates'),
-(4, null, 'Hit and Run'),
-(5, null, 'Lama Jar'),
-(6, null, 'Mountain Oysters'),
-(7, null, 'Death and the Maiden'),
-(8, 5, 'Donald Stumps'),
-(9, null, 'Slowboys')
+(1, null, 'Out of Control')
+,(2, null, 'Designated Drinkers')
+,(3, null, 'The Pirates')
+,(4, null, 'Hit and Run')
+,(5, null, 'Lama Jar')
+,(6, null, 'Mountain Oysters')
+,(7, null, 'Death and the Maiden')
+,(8, 5, 'Donald Stumps')
+,(9, null, 'Slowboys')
+,(10, null, 'Wizardsleeves')
+,(11, null, 'Split Fingers')
+,(12, null, 'Kanye Williamson')
+,(13, null, 'Squirt Squad')
+,(14, null, 'Ricoh Master Batters')
